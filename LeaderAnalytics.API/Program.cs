57b89fc9c339ea3f0762341b77c7611e9d23fbef
Sam <sam.wheat@outlook.com>
2020-05-20 14:54:59 -0700

    Add logging

--- LeaderAnalytics.API/Program.cs
+++ LeaderAnalytics.API/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace LeaderAnalytics.API
 {
@@ -15,13 +16,20 @@ namespace LeaderAnalytics.API
     {
         public static void Main(string[] args)
         {
+            // Create logs in D:\home\serilog
+            Log.Logger = new LoggerConfiguration()
+               .WriteTo.File("..\\..\\serilog\\log", rollingInterval: RollingInterval.Day, restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information)
+               .CreateLogger();
+            Log.Information("Logger created");
+            Log.Information("ConfigureServices started");
+
             try
             {
                 CreateHostBuilder(args).Build().Run();
             }
             catch (Exception ex)
-            { 
-            
+            {
+                Log.Error(ex.ToString());
             }
         }
 
